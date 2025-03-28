56b1e7581e1e822e67587e4547bc67f7d00ba2d9
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2025-03-28 13:27:36 +0100

    update

--- src/components/Cookies.jsx
+++ src/components/Cookies.jsx
@@ -1,21 +1,30 @@
 "use client";
+import { useState } from "react";
 import { LiaCookieBiteSolid } from "react-icons/lia";
 import { RxCross2 } from "react-icons/rx";
 
 //import { useState } from "react";
 
 const Cookies = () => {
+  const [cookiesAccepted, setCookiesAccepted] =
+    useState(false); /*Cookies er ikke accepteret endnu af brugeren*/
+  console.log(cookiesAccepted);
   return (
-    <div className="cookies">
-      <div className="icons">
-        <LiaCookieBiteSolid className="cookie-icon" />
-        <RxCross2 />
+    /*Gør at cookie-box forvinder når den er true */
+    !cookiesAccepted && (
+      <div className="cookies">
+        <div className="icons">
+          <LiaCookieBiteSolid className="cookie-icon" />
+          <RxCross2 />
+        </div>
+        <div>
+          <p>We use cookies to improve your user experience.</p>
+          <button onClick={() => setCookiesAccepted(!cookiesAccepted)}>
+            I like Cookies
+          </button>
+        </div>
       </div>
-      <div>
-        <p>We use cookies to improve your user experience.</p>
-        <button>I like Cookies</button>
-      </div>
-    </div>
+    )
   );
 };
 
